32d78035669296658c2768ac01754bd1e5db5661
ngadiono <ngadiono@djavaui.com>
2019-01-31 10:37:46 +0700

    add introduction

---
+++ vue-js-learning/introduction/declarative-rendering/app.js
@@ -0,0 +1,17 @@
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  });
+  
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Learn Javascript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  });
